ea69a7531077785ae9e3445b8eebcebdd8441410
yh725 <yh725@kakao.com>
2023-03-25 22:21:12 +0900

    스프링 타입 컨버터 - 프로젝트 생성 - 스프링 타입 컨버터 소개 - 타입 컨버터(Converter) - 컨버전 서비스(ConversionService)

---
+++ src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,33 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import hello.typeconverter.type.IpPortToStringConverter;
+import hello.typeconverter.type.StringToIpPortConverter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConversionServiceTest {
+
+	@Test
+	void conversionServiceTest() {
+		//등록
+		DefaultConversionService conversionService = new DefaultConversionService();
+		conversionService.addConverter(new StringToIntegerConverter());
+		conversionService.addConverter(new IntegerToStringConverter());
+		conversionService.addConverter(new StringToIpPortConverter());
+		conversionService.addConverter(new IpPortToStringConverter());
+
+		//사용
+		assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+		assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+
+		IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+		assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+		String ipPortString = conversionService.convert(new IpPort("127.0.0.1", 8080), String.class);
+		assertThat(ipPortString).isEqualTo("127.0.0.1:8080");
+	}
+}

388d9b63536e5040add7fdea1b560c892b2289dd
yh725 <yh725@kakao.com>
2023-03-29 21:55:30 +0900

    스프링 타입 컨버터 - 포맷터(Formatter) - 포맷터를 지원하는 컨버전 서비스 - 포맷터 적용하기 - 스프링이 제공하는 기본 포맷터

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -2,6 +2,7 @@ package hello.typeconverter;
 
 import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import hello.typeconverter.type.IpPortToStringConverter;
 import hello.typeconverter.type.StringToIpPortConverter;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
-		registry.addConverter(new StringToIntegerConverter());
-		registry.addConverter(new IntegerToStringConverter());
+		//주석처리 우선순위
+//		registry.addConverter(new StringToIntegerConverter());
+//		registry.addConverter(new IntegerToStringConverter());
 		registry.addConverter(new StringToIpPortConverter());
 		registry.addConverter(new IpPortToStringConverter());
+
+		//추가
+		registry.addFormatter(new MyNumberFormatter());
 	}
 }
